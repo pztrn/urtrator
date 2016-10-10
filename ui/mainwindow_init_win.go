e817a8fe642df3d9f9c40da6739368c671fb9cce
pztrn <pztrn@pztrn.name>
2016-10-10 20:18:59 +0500

    Windows theming with default theme.

--- ui/mainwindow_init_win.go
+++ ui/mainwindow_init_win.go
@@ -7,4 +7,9 @@ import (
 
 func (m *MainWindow) initializeWin() {
     platform.HideConsole(true)
+
+    dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+
+    // ToDo: theming support and theme seletion in settings.
+    gtk.RCParse(dir + "/themes/MS-Windows/gtk-2.0/gtkrc")
 }
