6495e9073704ce0658d63a6692b810f7e0807a8c
Stanislav N. aka pztrn <pztrn@pztrn.name>
2016-11-21 03:32:22 +0500

    gdk_threads are deprecated, as well as glib_threads.

--- ui/mainwindow_init.go
+++ ui/mainwindow_init.go
@@ -11,7 +11,6 @@ import (
     "github.com/pztrn/urtrator/common"
 
     // Other
-    "github.com/mattn/go-gtk/gdk"
     "github.com/mattn/go-gtk/gdkpixbuf"
     "github.com/mattn/go-gtk/glib"
     "github.com/mattn/go-gtk/gtk"
@@ -19,10 +18,6 @@ import (
 
 // Main window initialization.
 func (m *MainWindow) Initialize() {
-
-    glib.ThreadInit(nil)
-    gdk.ThreadsInit()
-    gdk.ThreadsEnter()
 
     gtk.Init(nil)
 
@@ -171,7 +166,6 @@ func (m *MainWindow) Initialize() {
     ctx.Eventer.LaunchEvent("setToolbarLabelText", map[string]string{"text": "URTrator is ready."})
 
     gtk.Main()
-    gdk.ThreadsLeave()
 }
 
 // Events initialization.
